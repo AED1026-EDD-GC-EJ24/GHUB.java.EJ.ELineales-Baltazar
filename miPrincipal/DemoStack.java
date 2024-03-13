cf2b533d006f1fccbe8022dbaa521d464f51d5c6
BaltazarIT <159161138+BaltazarIT@users.noreply.github.com>
2024-03-13 15:43:21 +0000

    stack

---
+++ miPrincipal/DemoStack.java
@@ -0,0 +1,15 @@
+package miPrincipal;
+import java.util.Stack;
+public class Stack {
+    public static void main(String[] strgs) {
+        Stack<Integer> pila = new Stack<Integer>();
+        //Apilamos elementos
+        pila.push(2);
+        pila.push(3);
+        pila.push(4);
+        System.out.println("El tope de la pila es "+pila.peek());
+        //Desapilamos elementos
+        pila.pop();
+        System.out.println("El tope de la pila es "+pila.peek());
+    }
+}
